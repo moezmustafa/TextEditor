0cff30bb30bb6116ae51757107087ae0008bff84
MYO THIRI <sandarlamin.llm@gmail.com>
2020-10-04 03:13:38 +0900

    Add bold,italic,underline and text align features.

--- script.js
+++ script.js
@@ -12,6 +12,8 @@
  */
 function updateText(){
   // CODE GOES HERE
+  let text = document.getElementById('text-input').value;
+  document.getElementById('text-output').innerHTML = text;
 
 }
 
@@ -24,12 +26,18 @@ function updateText(){
  */
 function makeBold(elem){
   //CODE GOES HERE
+  //console.log(elem)
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('bold');
+  
 }
 
 /**
  * Toggle the italic class for the output text
  */
 function makeItalic(elem){
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('italic');
 }
 
 /**
@@ -40,6 +48,13 @@ function makeItalic(elem){
  */
 function makeUnderline(elem){
     //CODE GOES HERE
+    elem.classList.toggle('active');
+    let formattedText = document.getElementById('text-output');
+    if(formattedText.classList.contains('underline')){
+      formattedText.classList.remove('underline');
+    }else{
+      formattedText.classList.add('underline');
+    }
 }
 
 /**
@@ -50,4 +65,11 @@ function makeUnderline(elem){
  */
 function alignText(elem, alignType){
   // CODE GOES HERE
+  elem.classList.toggle('active');
+  document.getElementById('text-output').style.textAlign = alignType;
+  let buttonsList = document.getElementsByClassName('align');
+  for(let i=0;i<buttonsList.length;i++){
+    buttonsList[i].classList.remove('active');
+  }
+  elem.classList.add('active');
 }
